d00e2befe9240401bfc8bed275c91986d872e6cb
Artem Stepanenko <103100378+NureStepanenkoArtem@users.noreply.github.com>
2024-05-09 08:03:23 +0300

    Added some files more
    more

---
+++ Task2/SecureAndObserve.UI/Controllers/OrdersApiController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SecureAndObserve.Core.Domain.Entities;
+using SecureAndObserve.Core.ServiceContracts;
+using SecureAndObserve.Infrastructure.DbContext;
+
+namespace SecureAndObserve.UI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersApiController : ControllerBase
+    {
+        private IOrdersService _ordersService;
+
+        public OrdersApiController(IOrdersService ordersService)
+        {
+            _ordersService = ordersService;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Rank>>> OrdersExcel()
+        {
+            MemoryStream memoryStream = await _ordersService.GetOrdersExcel();
+            return File(memoryStream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "orders.xlsx");
+        }
+    }
+}
